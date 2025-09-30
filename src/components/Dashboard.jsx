128f6bd5ae99a02b62676384e0491fb53396fd56
ronak <ronakbhojayya@gmail.com>
2025-09-30 10:34:52 +0530

    first commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,7 +7,7 @@ function Dashboard() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        axios.get('http://localhost:5000/api/auth/dashboard', { withCredentials: true })
+        axios.get('https://loginauth-ix8k.onrender.com/api/auth/dashboard', { withCredentials: true })
             .then(res => setUser(res.data.user))
             .catch(() => navigate('/login'));
     }, [navigate]);
